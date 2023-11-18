81bd738705d710cfb4b2c5598a8e061cac6f7235
Your Name <youremail@example.com>
2023-11-18 11:34:42 +0200

    add related products

--- func/CalcPrice.ts
+++
@@ -1,9 +0,0 @@
-export function formatTotalPrice(prices: number[]) {
-  // Calculate the total price
-  const totalPrice = prices.reduce((sum, price) => sum + price, 0);
-
-  // Format the total price with two decimal places
-  const formattedTotalPrice = totalPrice.toFixed(2);
-
-  return formattedTotalPrice;
-}
